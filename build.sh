2b6a7227587bfa66393d0955b036e71cb37fed8e
Oleg Tyshchenko <oleg.tyshchenko@gmail.com>
2015-03-16 22:47:08 +0200

    -little fix in build script.

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 xcodebuild -target XamarinSoxBindings -sdk iphoneos8.2  
 xcodebuild -target XamarinSoxBindings -sdk iphonesimulator -arch i386
+mkdir universal
 lipo -create -output universal/libXamarinSoxBindings.a build/Release-iphoneos/libXamarinSoxBindings.a build/Release-iphonesimulator/libXamarinSoxBindings.a
