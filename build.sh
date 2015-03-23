937962e2ad81b161ac8c803375cc90b4bbf85358
Oleg Tyshchenko <oleg.tyshchenko@gmail.com>
2015-03-23 21:43:11 +0200

    update project to support arm64

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 xcodebuild -target XamarinSoxBindings -sdk iphoneos
-xcodebuild -target XamarinSoxBindings -sdk iphonesimulator -arch i386
+xcodebuild -target XamarinSoxBindings -sdk iphonesimulator -arch i386 
 mkdir universal
 lipo -create -output universal/libXamarinSoxBindings.a build/Release-iphoneos/libXamarinSoxBindings.a build/Release-iphonesimulator/libXamarinSoxBindings.a
